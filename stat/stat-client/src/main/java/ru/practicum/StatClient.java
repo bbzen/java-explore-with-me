38832f7dad44c285f44c56a6489597c46e7a8897
GagarinSN <gagarin.sn@yandex.ru>
2023-10-05 21:49:45 +0500

    feat: add StatClient

---
+++ stat/stat-client/src/main/java/ru/practicum/StatClient.java
@@ -0,0 +1,14 @@
+package ru.practicum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatClient
+{
+    public static void StatClient( String[] args )
+    {
+        SpringApplication.run(StatClient.class, args);
+
+    }
+}
---
+++ stat/stat-client/src/main/java/ru/practicum/client/StatClient.java
@@ -0,0 +1,67 @@
+package ru.practicum.client;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+import ru.practicum.dto.EndpointHitDto;
+
+import java.util.Map;
+
+@Service
+public class StatClient {
+    private static final String API_POST_PREFIX = "/hit";
+    private static final String API_GET_PREFIX = "/stats";
+    private final RestTemplate rest;
+
+    @Autowired
+    public StatClient(@Value("${ewm-server.url}") String serverUrl, RestTemplateBuilder builder) {
+        this.rest = builder.uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl))
+                .requestFactory(HttpComponentsClientHttpRequestFactory::new)
+                .build();
+    }
+
+    public ResponseEntity<Object> postHit(EndpointHitDto dto) {
+        return makeAndSendRequest(HttpMethod.POST, API_POST_PREFIX, null, dto);
+    }
+
+    public ResponseEntity<Object> getStats(@Nullable Map<String, Object> parameters)
+
+    private <T> ResponseEntity<Object> makeAndSendRequest(HttpMethod method, String path, @Nullable Map<String, Object> parameters, T body) {
+        HttpEntity<T> requestEntity = new HttpEntity<>(body);
+
+        ResponseEntity<Object> ewmServerResponse;
+        try {
+            if (parameters != null) {
+                ewmServerResponse = rest.exchange(path, method, requestEntity, Object.class, parameters);
+            } else {
+                ewmServerResponse = rest.exchange(path, method, requestEntity, Object.class);
+            }
+        } catch (HttpStatusCodeException e) {
+            return ResponseEntity.status(e.getStatusCode()).body(e.getResponseBodyAsByteArray());
+        }
+        return prepareClientResponse(ewmServerResponse);
+    }
+
+    private static ResponseEntity<Object> prepareClientResponse(ResponseEntity<Object> response) {
+        if (response.getStatusCode().is2xxSuccessful()) {
+            return response;
+        }
+
+        ResponseEntity.BodyBuilder responseBuilder = ResponseEntity.status(response.getStatusCode());
+
+        if (response.hasBody()) {
+            return responseBuilder.body(response.getBody());
+        }
+
+        return responseBuilder.build();
+    }
+}

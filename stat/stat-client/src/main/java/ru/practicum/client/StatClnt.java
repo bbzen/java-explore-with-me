85b38c641982eb3110fe2916e6696247b72d0534
GagarisnSN <gagarin.sn@yandex.ru>
2023-10-06 12:40:29 +0500

    feat: add some fix StatClient

--- stat/stat-client/src/main/java/ru/practicum/client/StatClnt.java
+++ stat/stat-client/src/main/java/ru/practicum/client/StatClnt.java
@@ -3,9 +3,7 @@ package ru.practicum.client;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
@@ -14,12 +12,12 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 import ru.practicum.dto.EndpointHitDto;
 
+import java.util.List;
 import java.util.Map;
 
 @Service
 public class StatClnt {
     private static final String API_POST_PREFIX = "/hit";
-    private static final String API_GET_PREFIX = "/stats";
     private final RestTemplate rest;
 
     @Autowired
@@ -29,16 +27,20 @@ public class StatClnt {
                 .build();
     }
 
-    public ResponseEntity<Object> postHit(EndpointHitDto dto) {
-        return makeAndSendRequest(HttpMethod.POST, API_POST_PREFIX, null, dto);
+    public void postHit(EndpointHitDto dto) {
+        makeAndSendRequest(HttpMethod.POST, API_POST_PREFIX, null, dto);
     }
 
     public ResponseEntity<Object> getStats(@Nullable Map<String, Object> parameters) {
-        return makeAndSendRequest(HttpMethod.GET, API_GET_PREFIX, parameters, null);
+        String getPrefix = "/stats?start={start}&end={end}&unique={unique}";
+        if (parameters.get("uris") != null) {
+            getPrefix = "/stats?start={start}&end={end}&uris={uri}&unique={unique}";
+        }
+        return makeAndSendRequest(HttpMethod.GET, getPrefix, parameters, null);
     }
 
     private <T> ResponseEntity<Object> makeAndSendRequest(HttpMethod method, String path, @Nullable Map<String, Object> parameters, @Nullable T body) {
-        HttpEntity<T> requestEntity = new HttpEntity<>(body);
+        HttpEntity<T> requestEntity = new HttpEntity<>(body, defaultHeaders());
 
         ResponseEntity<Object> ewmServerResponse;
         try {
@@ -51,6 +53,13 @@ public class StatClnt {
             return ResponseEntity.status(e.getStatusCode()).body(e.getResponseBodyAsByteArray());
         }
         return prepareClientResponse(ewmServerResponse);
+    }
+
+    private HttpHeaders defaultHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
+        return headers;
     }
 
     private static ResponseEntity<Object> prepareClientResponse(ResponseEntity<Object> response) {
--- stat/stat-client/src/main/java/ru/practicum/controller/StatController.java
+++ stat/stat-client/src/main/java/ru/practicum/controller/StatController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import ru.practicum.client.StatClnt;
 import ru.practicum.dto.EndpointHitDto;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -21,13 +22,12 @@ public class StatController {
     }
 
     @GetMapping("/stats")
-    public ResponseEntity<Object> getStats(@RequestParam String start, @RequestParam String end, @RequestParam(required = false) String[] uris, @RequestParam(required = false, defaultValue = "false") Boolean unique) {
-        Map<String, Object> params = Map.of(
-                "start", start,
-                "end", end,
-                "uris", uris,
-                "unique", unique
-        );
+    public ResponseEntity<Object> getStats(@RequestParam String start, @RequestParam String end, @RequestParam(required = false) String[] uris, @RequestParam(defaultValue = "false") Boolean unique) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("start", start);
+        params.put("end", end);
+        params.put("uris", uris);
+        params.put("unique", unique);
         return statClnt.getStats(params);
     }
 }

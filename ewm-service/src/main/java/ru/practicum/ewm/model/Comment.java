125eb265c48b126068ddaaa94b81c00c457f887e
GagarisnSN <gagarin.sn@yandex.ru>
2023-10-23 10:29:53 +0500

    feat: add comments entity and repository

---
+++ ewm-service/src/main/java/ru/practicum/ewm/model/Comment.java
@@ -0,0 +1,30 @@
+package ru.practicum.ewm.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "comments", schema = "public")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "author", nullable = false)
+    private User author;
+    @ManyToOne
+    @JoinColumn(name = "event", nullable = false)
+    private Event event;
+    private String text;
+    private LocalDateTime timestamp;
+}
